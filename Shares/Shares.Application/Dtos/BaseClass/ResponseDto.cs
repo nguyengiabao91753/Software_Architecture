5e0577c0f848ec6e00ff667d12580c506867c4e4
Bao <nguyengiabao91753@gmail.com>
2025-09-28 22:22:09 +0700

    Tạo baseclass

---
+++ Shares/Shares.Application/Dtos/BaseClass/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shares.Application.Dtos.BaseClass;
+public class ResponseDto
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public object? Result { get; set; }
+    public string? Code { get; set; }
+}
